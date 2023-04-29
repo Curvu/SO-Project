d3f430613a855578e5867cff3b7a856f73dea0f2
Curvu <curvu6@gmail.com>
2023-04-29 14:18:09 +0100

    new functions - checkAlert - cpyAlert - searchAlert - cpyStat - searchStat - searchSensor - struct Stat - struct Message

--- lib/functions.c
+++ lib/functions.c
@@ -8,6 +8,10 @@ int isAlpha(char c) {
   return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
 }
 
+int isDigit(char c) {
+  return c >= '0' && c <= '9';
+}
+
 int verifyLength(char *str) {
   int length = strlen(str);
   return length >= 3 && length <= 32;
@@ -16,14 +20,14 @@ int verifyLength(char *str) {
 int verifyID(char *id) {
   if (!verifyLength(id)) return 0;
   int length = strlen(id);
-  for (int i = 0; i < length; i++) if (!isAlpha(id[i])) return 0;
+  for (int i = 0; i < length; i++) if (!isAlpha(id[i]) && !isDigit(id[i])) return 0;
   return 1;
 }
 
 int verifyKey(char *key) {
   if (!verifyLength(key)) return 0;
   int length = strlen(key);
-  for (int i = 0; i < length; i++) if (!isAlpha(key[i]) && !(key[i] == '_')) return 0;
+  for (int i = 0; i < length; i++) if (!isDigit(key[i]) && !isAlpha(key[i]) && !(key[i] == '_')) return 0;
   return 1;
 }
 
@@ -62,6 +66,48 @@ void cpySensor(Sensor *s1, Sensor *s2) {
   s1->inter = s2->inter;
 }
 
+int searchSensor(Sensor** sensors, Sensor* s, int len, int flag) {
+  for (int i = 0; i < len; i++) {
+    if ((compareSensors(sensors[i], s) && !flag) || (checkSensor(sensors[i], s->id, s->key) && flag)) return i;
+  }
+  return -1;
+}
+
 int compareAlerts(Alert *a1, Alert *a2) {
-  return (strcmp(a1->id, a2->id) == 0) && (strcmp(a1->key, a2->key) == 0) && (a1->min == a2->min) && (a1->max == a2->max);
+  return (strcmp(a1->id, a2->id) == 0) && (strcmp(a1->key, a2->key) == 0) && (a1->min == a2->min) && (a1->max == a2->max) && (a1->user == a2->user);
+}
+
+int checkAlert(Alert *a, char *id) {
+  return strcmp(a->id, id) == 0;
+}
+
+void cpyAlert(Alert *a1, Alert *a2) {
+  strcpy(a1->id, a2->id);
+  strcpy(a1->key, a2->key);
+  a1->min = a2->min;
+  a1->max = a2->max;
+  a1->user = a2->user;
+}
+
+int searchAlert(Alert** alerts, Alert* a, int len, int flag) {
+  for (int i = 0; i < len; i++) {
+    if ((compareAlerts(alerts[i], a) && !flag) || (checkAlert(alerts[i], a->id) && flag)) return i;
+  }
+  return -1;
+}
+
+void cpyStat(Stat *s1, Stat *s2) {
+  strcpy(s1->key, s2->key);
+  s1->min = s2->min;
+  s1->max = s2->max;
+  s1->last = s2->last;
+  s1->avg = s2->avg;
+  s1->count = s2->count;
+}
+
+int searchStat(Stat** stats, char *k, int len) {
+  for (int i = 0; i < len; i++) {
+    if (strcmp(stats[i]->key, k) == 0) return i;
+  }
+  return -1;
 }
--- lib/functions.h
+++ lib/functions.h
@@ -2,8 +2,18 @@
 #define FUNCTIONS_H
 
 #define DEBUG
+
+#define MESSAGE_QUEUE 123456789
+
+#define BUFFER 2048
 #define MAX 200
 #define STR 33
+
+typedef struct {
+  char key[STR];
+  int last, min, max, count;
+  double avg;
+} Stat;
 
 typedef struct {
   char id[STR], key[STR];
@@ -12,17 +22,23 @@ typedef struct {
 
 typedef struct {
   char id[STR], key[STR];
-  int min, max;
+  int min, max, user;
 } Alert;
 
 typedef struct {
   int user;
   int command;
   Alert alert;
+} Command;
+
+typedef struct {
+  long type; // this will be the id of the user_console
+  char response[BUFFER];
 } Message;
 
 #define NULL_SENSOR (Sensor) { "", "", 0, 0, 0 }
-#define NULL_ALERT (Alert) { "", "", 0, 0 }
+#define NULL_ALERT (Alert) { "", "", 0, 0, 0 }
+#define NULL_STAT (Stat) {"", 0, 0, 0, 0, 0 }
 
 #define SENSOR_FIFO "sensor_fifo"
 #define USER_FIFO "user_fifo"
@@ -74,11 +90,21 @@ int compareSensors(Sensor *, Sensor *);
 int checkSensor(Sensor *, char *, char *);
 
 /**
- * @brief copy a s2 to s1 
+ * @brief copy s2 to s1 
  * @param Sensor* s1
  * @param Sensor* s2
  */
 void cpySensor(Sensor *, Sensor *);
+
+/**
+ * @brief Search sensor in sensor list
+ * @param Sensor** sensors
+ * @param Sensor* s
+ * @param int len
+ * @param int flag - 0 if it's comparing all vars, 1 if comparing id and key
+ * @return return index if finds, -1 otherwise
+ */
+int searchSensor(Sensor**, Sensor*, int, int);
 
 /**
  * @brief compare two alerts
@@ -87,5 +113,46 @@ void cpySensor(Sensor *, Sensor *);
  * @return 1 if a1 == a2, 0 otherwise
  */
 int compareAlerts(Alert *, Alert *);
+
+/**
+ * @brief check if sensor has the same id
+ * @param Sensor* s
+ * @param char* id
+ * @return 1 if it's the same 0 otherwise
+ */
+int checkAlert(Alert *, char *);
+
+/**
+ * @brief copy a2 to a1 
+ * @param Sensor* a1
+ * @param Sensor* a2
+ */
+void cpyAlert(Alert *, Alert *);
+
+/**
+ * @brief Search alert in alert list
+ * @param Alert** alerts
+ * @param Alert* a
+ * @param int len
+ * @param int flag - 0 if it's comparing all vars, 1 if comparing only id
+ * @return return index if finds, -1 otherwise
+ */
+int searchAlert(Alert**, Alert*, int, int);
+
+/**
+ * @brief copy k2 to k1 
+ * @param Stat* s1
+ * @param Stat* s2
+ */
+void cpyStat(Stat *, Stat *);
+
+/**
+ * @brief Search key in stat list
+ * @param Stat** stats
+ * @param char* k
+ * @param int len
+ * @return return index if finds, -1 otherwise
+ */
+int searchStat(Stat**, char *, int);
 
 #endif // FUNCTIONS_H
